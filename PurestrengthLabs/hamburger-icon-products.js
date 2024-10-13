d837fd71d62ecff54092263d93f1b15dbd3e723c
alexdossss <amanabat1927@gmail.com>
2024-10-13 21:22:47 +0800

    Alexander

---
+++ PurestrengthLabs/hamburger-icon-products.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const menu = document.getElementById('phone-inner-links');
+    const hamburger = document.getElementById('hamburger-toggle-picture');
+
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+
+    if (hamburger.src.includes('hamburger-icon.png')) {
+        hamburger.src = '../media/icons/hamburger-close.png';
+    } else {
+        hamburger.src = '../media/icons/hamburger-icon.png'; 
+    }
+
+    hamburger.classList.toggle('active');
+}
